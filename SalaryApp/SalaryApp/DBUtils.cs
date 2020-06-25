b70faf74031f0f4a78f6f2a17431619ca29e54dc
PyankovAlexander <spider-aleks@yandex.ru>
2020-06-26 01:34:30 +0500

    Uploaded Project Files

---
+++ SalaryApp/SalaryApp/DBUtils.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SalaryApp
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "91.191.231.76";
+            int port = 3307;
+            string database = "pr_is207_18";
+            string username = "pr_is207_18";
+            string password = "pr_is207_18";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+    }
+}
